103b62f06d4268ce33a1456e7d2f1e6729afa5ee
udaya krishna karanam <udayakrishnakaranam@udayas-MacBook-Air.local>
2025-03-03 20:02:48 -0600

    Initial commit: Added Azure Function backend

--- sayogi_backend/getcaretakers/function_app.py
+++ sayogi_backend/getcaretakers/function_app.py
@@ -1,6 +1,17 @@
 import azure.functions as func
-import datetime
 import json
-import logging
 
 app = func.FunctionApp()
+
+@app.function_name(name="getcaretakers")
+@app.route(route="getcaretakers", auth_level=func.AuthLevel.ANONYMOUS)
+def get_caretakers(req: func.HttpRequest) -> func.HttpResponse:
+    """Fetch the list of caretakers"""
+    
+    caretakers = [
+        {"id": 1, "name": "Sarah Johnson", "location": "New York", "experience": "5 years"},
+        {"id": 2, "name": "David Smith", "location": "Los Angeles", "experience": "3 years"}
+    ]
+    
+    return func.HttpResponse(json.dumps(caretakers), mimetype="application/json")
+ß
